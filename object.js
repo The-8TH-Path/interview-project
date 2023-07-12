bdf6561683340e4bb18c4dfe60499b77253acb7b
K. Tanasrithatchai <korn@Ks-MacBook-Pro.local>
2023-07-12 15:09:25 +0700

    update code

--- object.js
+++ object.js
@@ -1,15 +1,3 @@
-// const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
-	
-// function toObject(arr) {
-// 	let rv = {};
-// 	for (let i = 0; i < arr.length; ++i)
-// 		rv[i] = arr[i];
-// 	return rv;
-// }
-
-// console.log(toObject(arr));
-// console.log(JSON.stringify(toObject(arr)));
-
 let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
  
 let objArr = array.map(function (val, index) {
